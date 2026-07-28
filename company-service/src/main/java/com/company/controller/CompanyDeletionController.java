ce1d63db9d6ba859f67ea569ba13b024f26d8fe4
anurag01502 <anuragkumar150202@gmail.com>
2026-07-28 22:20:01 +0530

    save changes

---
+++ company-service/src/main/java/com/company/controller/CompanyDeletionController.java
@@ -0,0 +1,13 @@
+package com.company.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/company")
+@RestController
+public class CompanyDeletionController {
+
+	
+	
+	
+}
