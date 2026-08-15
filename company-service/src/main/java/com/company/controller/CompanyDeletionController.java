50c3f4b45c81d3c23f1ee26c203141af5e0c4f37
anurag01502 <anuragkumar150202@gmail.com>
2026-08-15 20:33:50 +0530

    update and delete dummy apis for company

--- company-service/src/main/java/com/company/controller/CompanyDeletionController.java
+++ company-service/src/main/java/com/company/controller/CompanyDeletionController.java
@@ -1,6 +1,8 @@
 package com.company.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/company")
@@ -8,6 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class CompanyDeletionController {
 
 	
-	
+	@GetMapping("/delete")
+	public String getMethodName(@RequestParam String param) {
+		return new String();
+	}
 	
 }
--- company-service/src/main/java/com/company/controller/CompanyRegistrationAndUpdationController.java
+++ company-service/src/main/java/com/company/controller/CompanyRegistrationAndUpdationController.java
@@ -2,9 +2,19 @@ package com.company.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RequestMapping("/company")
 @RestController
 public class CompanyRegistrationAndUpdationController {
+	
+	
+	@GetMapping("/update")
+	public String getMethodName(@RequestParam String param) {
+		return new String();
+	}
+	
 
 }
