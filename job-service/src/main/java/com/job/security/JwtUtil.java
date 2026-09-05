377429d4227837a74580bf443552785ab02359c4
anurag01502 <anuragkumar150202@gmail.com>
2026-09-05 16:28:49 +0530

    removed unsed filters

--- job-service/src/main/java/com/job/security/JwtUtil.java
+++ job-service/src/main/java/com/job/security/JwtUtil.java
@@ -2,9 +2,6 @@ package com.job.security;
 
 
 import java.nio.charset.StandardCharsets;
-import java.util.Date;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import io.jsonwebtoken.Jwts;
