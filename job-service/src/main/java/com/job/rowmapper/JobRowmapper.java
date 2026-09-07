a09779f3233a7c00aceeff8b5e1a389fbae0d96c
anurag01502 <anuragkumar150202@gmail.com>
2026-09-07 22:04:51 +0530

    corrected jobrowmapper

--- job-service/src/main/java/com/job/rowmapper/JobRowmapper.java
+++ job-service/src/main/java/com/job/rowmapper/JobRowmapper.java
@@ -12,27 +12,21 @@ public class JobRowmapper {
         dto.setJobId(job.getJobId());
         dto.setCompanyId(job.getCompanyId());
         dto.setCreatorId(job.getCreatorId());
-
         dto.setTitle(job.getTitle());
         dto.setDescription(job.getDescription());
-
         dto.setEmploymentType(job.getEmploymentType());
         dto.setWorkMode(job.getWorkMode());
-
         dto.setExperienceMin(job.getExperienceMin());
         dto.setExperienceMax(job.getExperienceMax());
-
         dto.setSalaryMin(job.getSalaryMin());
         dto.setSalaryMax(job.getSalaryMax());
         dto.setSalaryCurrency(job.getSalaryCurrency());
-
         dto.setLocation(job.getLocation());
         dto.setOpenings(job.getOpenings());
         dto.setStatus(job.getStatus());
-
         dto.setApplicationDeadline(job.getApplicationDeadline());
-
-
+        dto.setCreatedAt(job.getCreatedAt());
+        dto.setUpdatedAt(job.getUpdatedAt());
 
         return dto;
     }
@@ -44,24 +38,18 @@ public class JobRowmapper {
         job.setJobId(dto.getJobId());
         job.setCompanyId(dto.getCompanyId());
         job.setCreatorId(dto.getCreatorId());
-
         job.setTitle(dto.getTitle());
         job.setDescription(dto.getDescription());
-
         job.setEmploymentType(dto.getEmploymentType());
         job.setWorkMode(dto.getWorkMode());
-
         job.setExperienceMin(dto.getExperienceMin());
         job.setExperienceMax(dto.getExperienceMax());
-
         job.setSalaryMin(dto.getSalaryMin());
         job.setSalaryMax(dto.getSalaryMax());
         job.setSalaryCurrency(dto.getSalaryCurrency());
-
         job.setLocation(dto.getLocation());
         job.setOpenings(dto.getOpenings());
         job.setStatus(dto.getStatus());
-
         job.setApplicationDeadline(dto.getApplicationDeadline());
 
         return job;
